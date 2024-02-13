d30cf0c29c4615990f6e25176f67c6c40630d8cf
j4v13rbr4y4nTheJavi <70727399+BrayanJcr@users.noreply.github.com>
2024-02-12 22:15:14 -0500

    Add files via upload

---
+++ SanValentin/Carta/script.js
@@ -0,0 +1,8 @@
+function mostrarMensaje() {
+    document.getElementById("bgoverlay").classList.add("open");
+}
+
+function reiniciar() {
+    document.getElementById("bgoverlay").classList.remove("open");
+}
+
